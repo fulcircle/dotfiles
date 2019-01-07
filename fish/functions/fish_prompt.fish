13639d5318c5207d5b52e65901608390a77b9e3a
Vik Sunkavalli <vsunkavalli@gmail.com>
2019-01-06 21:09:33 -0500

    Update oomox and fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -18,27 +18,27 @@ function fish_prompt
   # Setup colors
   set -l normal (set_color normal)
   set -l red (set_color red)
-  set -l cyan (set_color cyan)
-  set -l white (set_color white)
+  set -l white (set_color d6d0f6)
+  set -l highlight (set_color e92092)
 
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color white
-  set -g __fish_git_prompt_color_flags red
-  set -g __fish_git_prompt_color_prefix cyan
-  set -g __fish_git_prompt_color_suffix cyan
+  set -g __fish_git_prompt_color d6d0f6
+  set -g __fish_git_prompt_color_flags e92092
+  set -g __fish_git_prompt_color_prefix e92092
+  set -g __fish_git_prompt_color_suffix d6d0f6
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_showstashstate true
   set -g __fish_git_prompt_show_informative_status true
   
   # Line 1
-  echo -n $cyan'┌['$white$USER$cyan'@'$white$__fish_prompt_hostname$cyan']'$white'-'$cyan'('$white(prompt_pwd)$cyan')'
+  echo -n $highlight'┌['$white$USER$highlight'@'$white$__fish_prompt_hostname$highlight']'$white'-'$highlight'('$white(prompt_pwd)$highlight')'
   __fish_git_prompt "-[git://%s]-"
 
   echo
 
   # Line 2
-  echo -n $cyan'    └'$__fish_prompt_char $normal
+  echo -n $highlight'    └'$__fish_prompt_char $normal
 
 end
