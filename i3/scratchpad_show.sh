f2f88bec2e73ce1b779cef05e417e31f7a08475d
Vik Sunkavalli <vsunkavalli@gmail.com>
2023-12-12 18:29:25 -0600

    update scratchpad script

--- i3/scratchpad_show.sh
+++ i3/scratchpad_show.sh
@@ -1,18 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $2 == "class" ]; then
-	windows=( $(xdotool search --classname "$3") )
-elif [ $2 == "instance" ]; then
-	windows=( $(xdotool search --classname "$3") )
-elif [ $2 == "title" ]; then
-	windows=( $(xdotool search --name "$3") )
+if [ $# -ne 2 ]; then
+    echo "Usage: "${0}" <i3_mark> <launch_cmd>"
+    echo "Example: ${0} 'scratch-emacs' 'emacsclient -c -a emacs'"
+    exit 1
 fi
 
-if [ "${#windows[@]}" -eq "0" ]; then
-	i3-msg "exec --no-startup-id $1"
-	sleep 0.5
-	i3-msg "[$2=$3] move scratchpad"
+I3_MARK=${1}
+LAUNCH_CMD=${2}
+
+scratchpad_show() {
+    i3-msg "[con_mark=${I3_MARK}]" scratchpad show
+}
+
+# try showing the scratchpad window
+if ! scratchpad_show; then
+    # if there is no such window...
+
+    # launch the application.
+    eval "${LAUNCH_CMD}" &
+
+    # Wait for the next window event.
+    i3-msg -t subscribe  '[ "window" ]'
+
+    # Set a mark
+    i3-msg mark ${I3_MARK}
+
+    # Move it to the scratchpad workspace
+    i3-msg move scratchpad
+
+    # show the scratchpad window
+    scratchpad_show
 fi
-
-i3-msg "[$2=$3] $4, scratchpad show"
-
